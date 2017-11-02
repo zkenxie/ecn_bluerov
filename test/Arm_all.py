d75f62f1f654651191e5c4f767ab60ee0009ec63
ECN BlueROV <no@mail.uw>
2017-11-02 17:38:19 +0000

    modified Python - arm all

---
+++ test/Arm_all.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+import navio2.pwm
+import navio2.util
+
+navio2.util.check_apm()
+
+PWM_OUTPUTs = [2,1,3,5,8,10]
+#PWMs = [navio2.pwm.PWM(PWM_OUTPUT PWM_OUTPUT in PWM_OUTPUTs]
+
+enabled = False
+
+while True:
+    for PWM_OUTPUT in PWM_OUTPUTs:
+        with navio2.pwm.PWM(PWM_OUTPUT) as pwm:
+            if not enabled:
+                enabled = True
+                pwm.set_period(50)
+                pwm.enable()
+            pwm.set_duty_cycle(1.500)
+    time.sleep(1)
+        
